db7f1b12ec379e10725b33f85a332cf3864a3e80
nylonhat <70188956+nylonhat@users.noreply.github.com>
2024-09-27 15:54:14 +0930

    Update pure.h

--- src/pure.h
+++ src/pure.h
@@ -2,7 +2,7 @@
 #define PURE_H
 
 template<class R, class T>
-struct pure_op {
+struct PureOp {
         [[no_unique_address]] R recvr;
         T value;
 
@@ -12,22 +12,22 @@ struct pure_op {
 };
 
 template<class T>
-struct pure_sender {
+struct PureSender {
         using value_t = std::tuple<T>;
         T value;
 
         template<class R>
         auto connect(R recvr){
-                return pure_op{recvr, value};
+                return PureOp{recvr, value};
         }
 };
 
 auto pure = [](auto value){
-        return pure_sender{value};
+        return PureSender{value};
 };
 
 template<class R, class A, class B>
-struct pure2_op {
+struct Pure2Op {
         [[no_unique_address]] R recvr;
         A a;
         B b;
@@ -38,19 +38,19 @@ struct pure2_op {
 };
 
 template<class A, class B>
-struct pure2_sender {
+struct Pure2Sender {
         using value_t = std::tuple<A, B>;
         A a;
         B b;
 
         template<class R>
         auto connect(R recvr){
-                return pure2_op{recvr, a, b};
+                return Pure2Op{recvr, a, b};
         }
 };
 
 auto pure2 = [](auto a, auto b){
-        return pure2_sender{a, b};
+        return Pure2Sender{a, b};
 };
 
 #endif//PURE_H
