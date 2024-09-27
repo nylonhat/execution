2648f97d6ecf9faa89aa10fcd46445edef99af4b
nylonhat <70188956+nylonhat@users.noreply.github.com>
2024-09-27 16:07:10 +0930

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include "bind.h"
 #include "branch.h"
-#include "threadpool.h"
-#include <type_traits>
+#include "pure.h"
+#include "sync.h"
+#include "threadpool.h"
 #include <print>
 
 Threadpool pool{1};
@@ -16,34 +17,20 @@ auto add5 = [](int a) -> int{
 
 
 int main(){
-	//int a = pure(12) >> pure >> pure >> pure >> pure >> pure | sync_start;
+	//int a = pure(12) >> pure >> pure >> pure >> pure >> pure | sync;
 
-	//Sender auto b =branch(pool, pure(12), pure(5));
+	//Sender auto b = branch(pool, pure(12), pure(5));
 	Sender auto b = pure2(9, 11);
 	Sender auto c = b >> pure2;
 	Sender auto d = c > add;
 	Sender auto e = d >> pure;
 	Sender auto f = e > add5;
 	Sender auto g = f >> pure;
-	int h = g | sync_start;
+	int h = g | sync;
 
-	//int g = c | sync_start;
-	//int e = d | sync_start;
+	//int g = c | sync;
+	//int e = d | sync;
 
-
-	//static_assert(std::is_same_v<typename decltype(c)::value_t, int>);
-
-	//d.connect(noop_recvr{});
-
-	//auto f = ((pure(3) > add5) >> pure) | sync_start;
-	
-	//Sender auto b = branch(pool, pure(12), pure(5)) > add5 ;
-
-	//auto t = branch(pool, pure(12), pure(5)) > add;
-	//auto u = branch(pool, t, pure(7));
-	//auto o = connect(u, noop_recvr{});
-	
-	//std::println("{}", sizeof(o));
 
 	return 0 ;
 
