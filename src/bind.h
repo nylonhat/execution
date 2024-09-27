a3247f78f059fad207fc176434a0257a01900432
nylonhat <70188956+nylonhat@users.noreply.github.com>
2024-09-27 15:25:22 +0930

    Update bind.h

--- src/bind.h
+++ src/bind.h
@@ -5,35 +5,35 @@
 
 template<class F, class R>
 struct bind_recvr {
-    [[no_unique_address]] R end_recvr;
-    [[no_unique_address]] F mfunc;
+	[[no_unique_address]] R end_recvr;
+	[[no_unique_address]] F mfunc;
 
-    template<class... Args>
-    auto set_value(Args... args){
-        using S2 = std::invoke_result_t<F, Args...>;
-        using Op2 = connect_t<S2, R>;
+	template<class... Args>
+	auto set_value(Args... args){
+		using S2 = std::invoke_result_t<F, Args...>;
+		using Op2 = connect_t<S2, R>;
 		
-        S2 sender2 = std::invoke(mfunc, args...);
-        Op2& op2 = *reinterpret_cast<Op2*>(this);
-        op2 = connect(sender2, end_recvr);
-        start(op2);
-    }
+		S2 sender2 = std::invoke(mfunc, args...);
+		Op2& op2 = *reinterpret_cast<Op2*>(this);
+		op2 = connect(sender2, end_recvr);
+		start(op2);
+	}
 };
 
 
 template<class S1, class F, class R>
 struct bind_op {
-   	using BR = bind_recvr<F, R>;
-	   using Op1 = connect_t<S1, BR>;
+	using BR = bind_recvr<F, R>;
+	using Op1 = connect_t<S1, BR>;
 
-	   using S2 = apply_values_t<F, S1>; 
-	   using Op2 = connect_t<S2, R>;
+	using S2 = apply_values_t<F, S1>; 
+	using Op2 = connect_t<S2, R>;
 
-	   struct Param {
-        [[no_unique_address]] S1 sender1 = {};
-		      [[no_unique_address]] F mfunc = {};
-		      [[no_unique_address]] R end_recvr = {};
-   	};
+	struct Param {
+		[[no_unique_address]] S1 sender1 = {};
+		[[no_unique_address]] F mfunc = {};
+		[[no_unique_address]] R end_recvr = {};
+	};
 
 	   union {
 	      	Param init = {};
