40833f12ece83d042c1a61c6157b86d4a2be7668
nylonhat <nylonhat@gmail.com>
2024-10-19 17:16:22 +1030

    formatting

---
+++ etc/make-deps/threadpool.d
@@ -0,0 +1,8 @@
+bin/obj/threadpool.o: src/threadpool.cpp src/backoff.h src/threadpool.h \
+  src/queue.h src/deque.h src/scheduler.h src/sender.h
+src/backoff.h:
+src/threadpool.h:
+src/queue.h:
+src/deque.h:
+src/scheduler.h:
+src/sender.h:
--- src/pure.h
+++ src/pure.h
@@ -5,55 +5,55 @@
 
 template<class R, class T>
 struct PureOp {
-        [[no_unique_address]] R recvr;
-        T value;
+    [[no_unique_address]] R recvr;
+    T value;
 
-        void start(OpHandle op_handle){
-				recvr.set_value(op_handle, value);
-        }
+    void start(OpHandle op_handle){
+        recvr.set_value(op_handle, value);
+    }
 };
 
 template<class T>
 struct PureSender {
-        using value_t = std::tuple<T>;
-        T value;
+    using value_t = std::tuple<T>;
+    T value;
 
-        template<class R>
-        auto connect(R recvr){
-                return PureOp{recvr, value};
-        }
+    template<class R>
+    auto connect(R recvr){
+        return PureOp{recvr, value};
+    }
 };
 
 
 auto pure = []<class V>(V value){
-        return PureSender<V>{value};
+    return PureSender<V>{value};
 };
 
 template<class R, class A, class B>
 struct Pure2Op {
-        [[no_unique_address]] R recvr;
-        A a;
-        B b;
+    [[no_unique_address]] R recvr;
+    A a;
+    B b;
 
-        auto start(OpHandle op_handle){
-                recvr.set_value(op_handle, a, b);
-        }
+    auto start(OpHandle op_handle){
+        recvr.set_value(op_handle, a, b);
+    }
 };
 
 template<class A, class B>
 struct Pure2Sender {
-        using value_t = std::tuple<A, B>;
-        A a;
-        B b;
+    using value_t = std::tuple<A, B>;
+    A a;
+    B b;
 
-        template<class R>
-        auto connect(R recvr){
-                return Pure2Op{recvr, a, b};
-        }
+    template<class R>
+    auto connect(R recvr){
+        return Pure2Op{recvr, a, b};
+    }
 };
 
 auto pure2 = [](auto a, auto b){
-        return Pure2Sender{a, b};
+    return Pure2Sender{a, b};
 };
 
 #endif//PURE_H
