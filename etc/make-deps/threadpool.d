06833d563d109a2a8298ec23674f3eee587fcf54
nylonhat <nylonhat@gmail.com>
2024-10-19 17:16:52 +1030

    formatting

--- etc/make-deps/threadpool.d
+++
@@ -1,8 +0,0 @@
-bin/obj/threadpool.o: src/threadpool.cpp src/backoff.h src/threadpool.h \
-  src/queue.h src/deque.h src/scheduler.h src/sender.h
-src/backoff.h:
-src/threadpool.h:
-src/queue.h:
-src/deque.h:
-src/scheduler.h:
-src/sender.h:
