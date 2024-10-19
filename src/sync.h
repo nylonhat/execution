93cb725104f36d172f1758967d179c44e5ae778c
nylonhat <nylonhat@gmail.com>
2024-10-19 17:18:47 +1030

    formatting

--- src/sync.h
+++ src/sync.h
@@ -7,29 +7,29 @@
 
 template<class T>
 struct SyncRecvr {
-        T* value;
-        std::binary_semaphore* flag;
+    T* value;
+    std::binary_semaphore* flag;
 
-        auto set_value(OpHandle op_handle, auto v){
-                *value = v;
-                flag->release();
-				return op_handle.start({});
-        }
+    auto set_value(OpHandle op_handle, auto v){
+        *value = v;
+        flag->release();
+		return op_handle.start({});
+    }
 };
 
 auto sync_wait = []<class S>(S sender){
-        using T = single_value_t<S>;
-        T value;
-        std::binary_semaphore flag{0};
-        
-		auto sync_recvr = SyncRecvr{&value, &flag};
-        auto op = ::connect(sender, sync_recvr);
-		std::println("final op size: {} bytes", sizeof(op));
-		
-		::start(op, {});
+    using T = single_value_t<S>;
+    T value;
+    std::binary_semaphore flag{0};
+    
+	auto sync_recvr = SyncRecvr{&value, &flag};
+    auto op = ::connect(sender, sync_recvr);
+	std::println("final op size: {} bytes", sizeof(op));
+	
+	::start(op, {});
 
-        flag.acquire();
-        return value;
+    flag.acquire();
+    return value;
 };
 
 #endif//SYNC_H
